3a14bc7becd9af286b19812def7082f4f5e18f49
itamang <ishatamang17@gmail.com>
2026-05-13 09:14:22 +0800

    add toast for rate limiting

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {UploadWidgetValue} from "@/types";
+import {UploadWidgetProps, UploadWidgetValue} from "@/types";
 import {UploadCloud} from "lucide-react";
 import { CLOUDINARY_CLOUD_NAME, CLOUDINARY_UPLOAD_PRESET } from '@/constants';
 
-const UploadWidget = ({value = null , onChange, disabled= false}) => {
+const UploadWidget = ({value = null , onChange, disabled= false}: UploadWidgetProps) => {
     const widgetRef = useRef<CloudinaryWidget | null>(null);
     const onChangeRef = useRef(onChange);
 
